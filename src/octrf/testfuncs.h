03ed6414a73a9271bf40df2b7e6e118a18183089
tabe <abe.univ@gmail.com>
2011-10-31 20:12:25 +0900

    BinaryStamp for DV

--- src/octrf/testfuncs.h
+++ src/octrf/testfuncs.h
@@ -13,11 +13,14 @@ namespace octrf {
             BinaryStamp() : dim_(1), th_(0), d_(0){}
             BinaryStamp(const int dim, const int th = 0)
                 :  dim_(dim), th_(th), d_(0){};
-            bool operator() (const std::vector< std::pair<int, T> >& x) const {
+            bool operator() (const std::vector< std::pair<int, T> >& x) const { // for SV
                 for(int i=0; i < x.size(); i++){
                     if(x[i].first == d_) return x[i].second > th_;
                 }
                 return  0 > th_;
+            }
+            bool operator() (const std::vector<T>& x) const { // for DV
+                return x[d_] > th_;
             }
             void random_sample() { d_ = rand() % dim_; }
             std::string serialize() const {
